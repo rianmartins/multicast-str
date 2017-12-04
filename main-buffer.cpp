4d273fe89a782bcf7b370e1b3c96f79d60662828
rianmartins <rianlopes@hotmail.com>
2017-12-04 12:25:22 -0300

    main-buffer atualizada

--- main-buffer.cpp
+++ main-buffer.cpp
@@ -5,8 +5,6 @@
 #include <semaphore.h>
 #include <string.h>
 #include <time.h>
-#include "BlackGPIO/BlackGPIO.h"
-#include "ADC/Adc.h"
 #include <sys/types.h>
 #include <sys/socket.h>
 #include <netinet/in.h>
@@ -14,11 +12,9 @@
 
 #define MULTICAST_ADDR "255.0.0.37"
 
-using namespace BlackLib;
-
 void *thread_function_1(void *arg);
 void *thread_function_2(void *arg);
-void led_number(int k);
+int* led_number(int k);
 pthread_mutex_t t_mutex; 
 
 #define WORK_SIZE 1024
@@ -27,36 +23,56 @@ char work_area[WORK_SIZE];
 int buffer = 0;
 double tempo[2]= {1,1};
 
-BlackGPIO botao(GPIO_66,input);
-BlackGPIO pin1(GPIO_5,output);
-BlackGPIO pin2(GPIO_48,output);
-BlackGPIO pin3(GPIO_31,output);
-BlackGPIO pin4(GPIO_60,output);
-BlackGPIO pin5(GPIO_30,output);
-BlackGPIO pin6(GPIO_4,output);
-BlackGPIO pin7(GPIO_15,output);
-ADC adc_t1(AIN0);
-ADC adc_t2(AIN1);
-
 int main(){
     int res;
     pthread_t a_thread, b_thread;
     void *thread_result;
     int i=0;
-    socklen_t len_recv;
-    struct sockaddr_in address;
-    int sockfd;
-    int len;
-    int result;
+    
+    int server_sockfd, client_sockfd;
+    size_t server_len;
+    socklen_t client_len;
+    struct sockaddr_in server_address;
+    struct sockaddr_in client_address;
+    
+    struct ip_mreq mreq;  // para endere�o multicast
+    
+    unsigned short porta = 9711;
 
     res = pthread_mutex_init(&t_mutex, NULL);    
     if (res != 0) {
         perror("Iniciacao do Mutex falhou");
         exit(EXIT_FAILURE);
     }
+
+    unlink("server_socket");  // remocao de socket antigo
+    if ( (server_sockfd = socket(AF_INET, SOCK_DGRAM, 0) )  < 0  )  // cria um novo socket
+    {
+        printf(" Houve erro na ebertura do socket ");
+        exit(1);
+    }
+    server_address.sin_family = AF_INET;
+    server_address.sin_addr.s_addr = htonl(INADDR_ANY);
+    server_address.sin_port = htons(porta);
     
-    while(1){
-    i=0;
+    
+    server_len = sizeof(server_address);
+    
+    if(  bind(server_sockfd, (struct sockaddr *) &server_address, server_len) < 0 )
+    {
+        perror("Houve error no Bind");
+        exit(1);
+    }
+    
+    
+    // use setsockopt() para requerer inscri��o num grupo multicast
+    mreq.imr_multiaddr.s_addr=inet_addr(MULTICAST_ADDR);
+    mreq.imr_interface.s_addr=htonl(INADDR_ANY);
+    if (setsockopt(server_sockfd,IPPROTO_IP,IP_ADD_MEMBERSHIP,&mreq,sizeof(mreq)) < 0) {
+        perror("setsockopt");
+        exit(1);
+    }
+
     res = pthread_create(&a_thread, NULL, thread_function_1, NULL);
     if (res != 0) {
         perror("Criacao da Thread 1 falhou");
@@ -69,30 +85,13 @@ int main(){
         exit(EXIT_FAILURE);
     }
 
-    unsigned short porta = 9711;
-    sockfd = socket(AF_INET, SOCK_DGRAM, 0);
+    
+    while(1){
 
-    address.sin_family = AF_INET;
-    address.sin_addr.s_addr = inet_addr(MULTICAST_ADDR);
-    address.sin_port = htons(porta);    
-
-    while(i==0){
-	                
-        tempo[1]= adc_t1.getPercentValue()/100.0;
-        tempo[2]= adc_t2.getPercentValue()/100.0;
-	if(botao.getValue()!="1"){
-		pthread_cancel(a_thread);
-		pthread_cancel(b_thread);		
-		led_number(0);
-		pthread_mutex_unlock(&t_mutex);
-		i = 1;
-		buffer = 0 ;
+        recvfrom(server_sockfd, &tempo, sizeof(tempo),0,(struct sockaddr *) &client_address, &client_len);
+			        
 		sleep(1);
-		
-	}
-		        
-	sleep(0.5);
-   	}
+		                
     }
     printf("\nMAIN() --> Esperando as threads terminar...\n");
     
@@ -108,32 +107,71 @@ int main(){
 }
 
 void *thread_function_1(void *arg) {
+	
+	int *values;
+	values = malloc(8*sizeof(int));
+
+	int sockfd;
+    int len;
+    struct sockaddr_in address;
+    
+    unsigned short porta = 9811;
+    
+    sockfd  = socket(AF_INET, SOCK_DGRAM,0);  // criacao do socket
+    
+    address.sin_family = AF_INET;
+    address.sin_addr.s_addr = inet_addr(MULTICAST_ADDR);
+    address.sin_port = htons(porta);
+    
+    len = sizeof(address);
+
 	while(1){
 		pthread_mutex_lock(&t_mutex);
 		if(buffer < 7)
 			buffer = buffer + 1;
 		printf("buffer: %d\n", buffer);
 		if (buffer == 7)
-			led_number(7);
+			values = led_number(7);
 		else if (buffer == 6)
-			led_number(6);
+			values = led_number(6);
 		else if (buffer == 5)
-			led_number(5);			
+			values = led_number(5);			
 		else if (buffer == 4)
-			led_number(4);
+			values = led_number(4);
 		else if (buffer == 3)
-			led_number(3);
+			values = led_number(3);
 		else if (buffer == 2)
-			led_number(2);
+			values = led_number(2);
 		else if (buffer == 1)
-			led_number(1);
+			values = led_number(1);
 	
+		sendto(sockfd, &values,sizeof(values),0,(struct sockaddr *) &address, len);
 		pthread_mutex_unlock(&t_mutex);		
-		sleep(tempo[1]*5);		
+		sleep(tempo[0]*5);		
 	}
+
+	free(values);
+	close(sockfd);
 }
 
 void *thread_function_2(void *arg) {	
+	int *values;
+	values = malloc(8*sizeof(int));
+
+	int sockfd;
+    int len;
+    struct sockaddr_in address;
+    
+    unsigned short porta = 9811;
+    
+    sockfd  = socket(AF_INET, SOCK_DGRAM,0);  // criacao do socket
+    
+    address.sin_family = AF_INET;
+    address.sin_addr.s_addr = inet_addr(MULTICAST_ADDR);
+    address.sin_port = htons(porta);
+    
+    len = sizeof(address);
+
 	while(1){
 		pthread_mutex_lock(&t_mutex);
 		if(buffer > 0)
@@ -141,106 +179,123 @@ void *thread_function_2(void *arg) {
 		printf("buffer: %d\n", buffer);
 		
 		if (buffer == 6)
-			led_number(6);
+			values = led_number(6);
 		else if (buffer == 5)
-			led_number(5);			
+			values = led_number(5);			
 		else if (buffer == 4)
-			led_number(4);
+			values = led_number(4);
 		else if (buffer == 3)
-			led_number(3);
+			values = led_number(3);
 		else if (buffer == 2)
-			led_number(2);
+			values = led_number(2);
 		else if (buffer == 1)
-			led_number(1);
+			values = led_number(1);
 		else if (buffer == 0)
-			led_number(0);
+			values = led_number(0);
 
+		sendto(sockfd, &values,sizeof(values),0,(struct sockaddr *) &address, len);
 		pthread_mutex_unlock(&t_mutex);
-		sleep(tempo[2]*5);		
+		sleep(tempo[1]*5);		
 	}
+
+	free(values);
+
+	close(sockfd);
 }
 
-void led_number(int k) {
+int* led_number(int k) {
+
+	int *values;
+	values = malloc(8*sizeof(int));
+
 	if (k == 7)
 	{
-		pin1.setValue(low);
-		pin2.setValue(low);
-		pin3.setValue(low);
-		pin4.setValue(high);
-		pin5.setValue(low);
-		pin6.setValue(high);
-		pin7.setValue(high);
+		values[0] = 0;
+		values[1] = 0;
+		values[2] = 0;
+		values[3] = 0;
+		values[4] = 0;
+		values[5] = 0;
+		values[6] = 0;
+		values[7] = 1;
 	}
 	else if (k == 6)
 	{
-		pin1.setValue(high);
-		pin2.setValue(high);
-		pin3.setValue(high);
-		pin4.setValue(high);
-		pin5.setValue(high);
-		pin6.setValue(high);
-		pin7.setValue(low);
+		values[0] = 0;
+		values[1] = 0;
+		values[2] = 0;
+		values[3] = 0;
+		values[4] = 0;
+		values[5] = 0;
+		values[6] = 0;
+		values[7] = 1;
 	}
 	else if (k == 5)
 	{
-		pin1.setValue(high);
-		pin2.setValue(low);
-		pin3.setValue(high);
-		pin4.setValue(high);
-		pin5.setValue(high);
-		pin6.setValue(high);
-		pin7.setValue(low);
+		values[0] = 0;
+		values[1] = 0;
+		values[2] = 0;
+		values[3] = 0;
+		values[4] = 0;
+		values[5] = 0;
+		values[6] = 0;
+		values[7] = 1;
 	}
 	else if (k == 4)
 	{
-		pin1.setValue(low);
-		pin2.setValue(low);
-		pin3.setValue(high);
-		pin4.setValue(high);
-		pin5.setValue(high);
-		pin6.setValue(low);
-		pin7.setValue(high);
+		values[0] = 0;
+		values[1] = 0;
+		values[2] = 0;
+		values[3] = 0;
+		values[4] = 0;
+		values[5] = 0;
+		values[6] = 0;
+		values[7] = 1;
 	}
 	else if (k == 3)
 	{
-	 	pin1.setValue(high);
-		pin2.setValue(low);
-		pin3.setValue(high);
-		pin4.setValue(high);
-		pin5.setValue(low);
-		pin6.setValue(high);
-		pin7.setValue(high);
+		values[0] = 0;
+		values[1] = 0;
+		values[2] = 0;
+		values[3] = 0;
+		values[4] = 0;
+		values[5] = 0;
+		values[6] = 0;
+		values[7] = 1;
 	}
 	else if (k == 2)
 	{
-		pin1.setValue(high);
-		pin2.setValue(high);
-		pin3.setValue(high);
-		pin4.setValue(low);
-		pin5.setValue(low);
-		pin6.setValue(high);
-		pin7.setValue(high);
-
+		values[0] = 0;
+		values[1] = 0;
+		values[2] = 0;
+		values[3] = 0;
+		values[4] = 0;
+		values[5] = 0;
+		values[6] = 0;
+		values[7] = 1;
 	}
 	else if (k == 1)
 	{
-	 	pin1.setValue(low);
-		pin2.setValue(low);
-		pin3.setValue(low);
-		pin4.setValue(high);
-		pin5.setValue(low);
-		pin6.setValue(low);
-		pin7.setValue(high);
-
+		values[0] = 0;
+		values[1] = 0;
+		values[2] = 0;
+		values[3] = 0;
+		values[4] = 0;
+		values[5] = 0;
+		values[6] = 0;
+		values[7] = 1;
 	}
 	else if (k == 0)
 	{
-	 	pin1.setValue(high);
-		pin2.setValue(high);
-		pin3.setValue(low);
-		pin4.setValue(high);
-		pin5.setValue(high);
-		pin6.setValue(high);
-		pin7.setValue(high);
+		values[0] = 0;
+		values[1] = 0;
+		values[2] = 0;
+		values[3] = 0;
+		values[4] = 0;
+		values[5] = 0;
+		values[6] = 0;
+		values[7] = 1;
 	}
+
+	return values;
 }
